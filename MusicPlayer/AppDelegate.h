367f10ae1fec1830c623518a655a9ae325a3c8f9
Dimitar Haralanov <dharalanov@me.com>
2014-09-04 22:15:21 +0300

    Iinitial commit

---
+++ MusicPlayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MusicPlayer
+//
+//  Created by Dimitar Haralanov on 9/4/14.
+//  Copyright (c) 2014 Dimitar Haralanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MusicPlayer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MusicPlayer
+//
+//  Created by Dimitar Haralanov on 9/4/14.
+//  Copyright (c) 2014 Dimitar Haralanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
